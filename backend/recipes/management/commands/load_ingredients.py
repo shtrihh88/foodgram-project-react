7193f769b43fadce6a7237e3fbc5416c328b6125
shtrihh88 <shtrihh88@mail.ru>
2022-01-09 16:07:42 +0300

    some_fix

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -1,33 +1,16 @@
 import csv
-from itertools import islice
 
-from django.conf import settings
-from django.core.management import BaseCommand
-from recipes.models import Measurement
-
-BATCH_SIZE = 100
+from django.core.management.base import BaseCommand
+from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-    help = 'Read .csv'
+    help = 'Load ingredients data to DB'
 
-    def handle(self, *args, **kwargs):
-        project_dir = settings.BASE_DIR
-        with open(
-            f'{project_dir}/data/ingredients.csv',
-            'r', encoding='utf-8'
-        ) as csv_file:
-            reader = csv.DictReader(
-                csv_file, fieldnames=['name', 'measurement_unit']
-            )
-            objects = (Measurement(
-                name=line['name'],
-                measurement_unit=line['measurement_unit']) for line in reader
-            )
-            while True:
-                batch = list(islice(objects, BATCH_SIZE))
-                if not batch:
-                    break
-                Measurement.objects.bulk_create(batch, BATCH_SIZE)
-
-        self.stdout.write(self.style.SUCCESS('Ingredients loaded'))
+    def handle(self, *args, **options):
+        with open('recipes/data/ingredients.csv', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                name, unit = row
+                Ingredient.objects.get_or_create(
+                    name=name, measurement_unit=unit)
